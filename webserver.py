792f22b9ca264c21c5c8c414c0aa6d5743325f5a
Bruno Figueiredo <brun094_figueiredo@hotmail.com>
2017-11-13 12:08:20 -0800

    Modificação do Webserver para executar os comandos nos Daemons e aprimoramento das saídas.

--- webserver.py
+++ webserver.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from backend import *
 import cgitb
 import cgi
 cgitb.enable()
@@ -12,7 +13,7 @@ print("<head><title>Trabalho 1 de Redes</title></head>")
 # Para cada Daemon
 for j in range(1, 4):
 	commands = []
-	print("Daemon " + str(j) + ":<br><br>")
+	print("<h1>Daemon " + str(j) + ":</h1>")
 	# Para cada Comando
 	for i in range(4):
 		# Testa se o checkbox do comando foi marcado
@@ -23,6 +24,13 @@ for j in range(1, 4):
 				opt = " " + form["maq" + str(j) +"-" + fields[i]].value
 			# Adiona opt (parametros dos comandos) na lista de comandos a serem executado
 			commands.append((i + 1, opt))
-			# Imprime a lista de comandos a ser enviada para execucao
-			print(fields[i] + " " + opt + "<br><br>")
+	# Executa lista de comandos do Daemon
+	outputs = executeCommands(commands, j-1)
+
+	# Para cada Comando
+	for i in range(len(outputs)):
+		# Imprime o Comando executado e os seus paramentros
+		print("<h3>" + fields[commands[i][0] - 1] + " " + commands[i][1] + "</h3>")
+		# Imprime o Output do Comando
+		print(outputs[i].replace("\n", "<br>") + "<br><br>")
 
